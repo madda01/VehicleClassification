22aa46fdbcde05f08f98229b7f0984bdb0858cfd
madda01 <it20232368@my.sliit.lk>
2024-02-18 14:29:28 +0530

    reformatting the code

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import cvzone
 import cv2
 import os
 
+# Path to the folder containing test images
 folder_path = 'D:/ComputerVisionProj/vehicleClassification/test'
+
+# List all files in the folder with specified image file extensions
 image_files = [f for f in os.listdir(folder_path) if f.endswith(('.jpg', '.jpeg', '.png'))]
 
-capture = cv2.VideoCapture(0)
-
+# Initialize the classifier using a pre-trained model and labels
 myClassifier = cvzone.Classifier('MyModel/keras_model.h5', 'MyModel/labels.txt')
 
 print("""
@@ -18,13 +20,20 @@ print("""
     4 - Bicycles classification
     """)
 
+# Loop through each image in the specified folder
 for i in image_files:
+    # Read the image from the folder
     img = cv2.imread(os.path.join(folder_path, i))
 
+    # Resize the image for better display
     img = cv2.resize(img, (0, 0), fx=5, fy=5)
 
+    # Get predictions and index from the classifier
     predictions, index = myClassifier.getPrediction(img)
     print(predictions, index)
 
+    # Display the resized image
     cv2.imshow("Image", img)
+
+    # Wait for 2 seconds before moving to the next image
     cv2.waitKey(2000)
